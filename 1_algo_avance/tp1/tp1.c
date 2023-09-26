82504ca1f71fb715b997790d2ad894b51d4b9a1c
Valentin_G <valentin.guillon93430@gmail.com>
2023-09-26 06:19:31 +0200

    algo avancée, tp1: correction exo2 B

--- 1_algo_avance/tp1/tp1.c
+++ 1_algo_avance/tp1/tp1.c
@@ -15,21 +15,38 @@ int triangle_rec(int k, int n) {
 
 
 // B
-void triangle_it(int** tab, int k, int n) {
+void triangle_it(int** tab, int n) {
     tab[0][0] = 1;
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         tab[i][0] = 1;
-        for (int j = 1; j < i; j++) {
-           tab[i][j] = tab[i-1][j-1] + tab[i-1][j];
+        for (int j = 1; j <= n; j++) {
+            if (j > n) tab[i][j] = 0;
+            else tab[i][j] = tab[i-1][j-1] + tab[i-1][j];
         }
+        // tab[i][i] = 1;
     }
 }
 
-void aff(int **tab, int k, int n) {
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            printf("%d, ", tab[i][j]);
+void aff_value(int value) {
+    if (value > 99) printf(" ");
+    else if (value > 9) printf("  ");
+    else printf("   ");
+    printf("%d", value);
+}
+
+void aff(int **tab, int n) {
+    //decoration
+    printf("  n\\k|");
+    for (int i = 0; i <= n; i++) aff_value(i);
+    printf("\n");
+
+    //parcours et affichage des éléments de la matrice (uniquement quand i <= j)
+    for (int i = 0; i <= n; i++) {
+        aff_value(i); printf(" |"); //decoration
+        // for (int j = 0; j <= n; j++) {
+        for (int j = 0; j <= i; j++) {
+            aff_value(tab[i][j]);
         }
         printf("\n");
     }
@@ -42,34 +59,41 @@ void aff(int **tab, int k, int n) {
 
 
 void A(int k, int n) {
+    printf("\nEXO2 A\n");
     printf("%d\n", triangle_rec(k, n));
 }
 
-void B(int k, int n) {
+void B(int n) {
+    printf("\nEXO2 B\n");
     int **tab;
     
-    tab = malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
-        tab[i] = malloc(k * sizeof(int));
-        for (int j = 0; j < k; j++) {
-            tab[i][j] = 0;
-        }
+    tab = malloc((n+1) * sizeof(int *));
+    for (int i = 0; i <= n; i++) {
+        tab[i] = malloc((n+1) * sizeof(int));
     }
     
     
     
-    triangle_it(tab, k, n);
-    aff(tab, k, n);
+    triangle_it(tab, n);
+    aff(tab, n);
+}
+
+void C(int k, int n) {
+    printf("\nEXO2 C\n");
+    printf("NOT WRITE\n");
 }
 
 
 void exo2(void) {
     
-    int n = 8;
+    int n = 6;
     int k = 3;
+    
+    printf("n = %d, k = %d\n", n, k);
 
     A(k, n);
-    B(k, n);
+    B(n);
+    C(k, n);
     
 }
 
