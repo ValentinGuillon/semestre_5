5976880dc211315e2cf9da118c1dc0a96da84170
Valentin_G <valentin.guillon93430@gmail.com>
2023-09-28 07:53:48 +0200

    algo avancée, tp1: presque fini (sauf bonus). Il manque la comparaison des vitesse d'exé

--- 1_algo_avance/tp1/tp1.c
+++ 1_algo_avance/tp1/tp1.c
@@ -3,31 +3,13 @@
 
 
 
+typedef struct Vector_T {
+    int max_size;
+    int size;
+    int *tab;
+} Vector;
 
-
-// =====================
-// EXO 2
-// A
-int triangle_rec(int k, int n) {
-    if (k == 0 || n == k) return 1;
-    return triangle_rec(k-1, n-1) + triangle_rec(k, n-1);
-}
-
-
-// B
-void triangle_it(int** tab, int n) {
-    tab[0][0] = 1;
-    for (int i = 1; i <= n; i++) {
-        tab[i][0] = 1;
-        for (int j = 1; j <= n; j++) {
-            if (j > n) tab[i][j] = 0;
-            else tab[i][j] = tab[i-1][j-1] + tab[i-1][j];
-        }
-        // tab[i][i] = 1;
-    }
-}
-
-
+//affiche un entier (entre 0 et 999) à droite, sur 4 caractères 
 void aff_value(int value) {
     if (value > 99) printf(" ");
     else if (value > 9) printf("  ");
@@ -35,16 +17,60 @@ void aff_value(int value) {
     printf("%d", value);
 }
 
-void aff(int **tab, int n) {
+void init_vector(Vector* vec, int max) {
+    vec->max_size = max;
+    vec->size = 0;
+    vec->tab = malloc(max * sizeof(int));
+}
+
+void print_vector_tab(Vector* vec) {
+    for (int i = 0; i < vec->size; i++) {
+        aff_value(vec->tab[i]);
+    }
+}
+
+
+
+
+
+
+// =====================
+// EXO 2
+// A
+//calcule le résultat d'UN SEUL couple k n, récursivement
+int triangle_rec(int k, int n) {
+    if (k == 0 || n == k) return 1;
+    return triangle_rec(k-1, n-1) + triangle_rec(k, n-1);
+}
+
+
+
+
+// B
+//calcule TOUS les résultat tous les couples k n, et place le résultat de chacun dans un tableau de taille n*n
+void triangle_it_2d(int** tab, int n) {
+    // ligne de 0 à n
+    for (int i = 0; i <= n; i++) {
+        // colonne de 0 à i
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i)
+                tab[i][j] = 1;
+            else
+                tab[i][j] = tab[i-1][j-1] + tab[i-1][j];
+        }
+    }
+}
+
+
+void print_tab_2d(int **tab, int n) {
     //decoration
-    printf("  n\\k|");
-    for (int i = 0; i <= n; i++) aff_value(i);
-    printf("\n");
+    //printf("  n\\k|");
+    //for (int i = 0; i <= n; i++) aff_value(i);
+    //printf("\n");
 
     //parcours et affichage des éléments de la matrice (uniquement quand i <= j)
     for (int i = 0; i <= n; i++) {
-        aff_value(i); printf(" |"); //decoration
-        // for (int j = 0; j <= n; j++) {
+        //aff_value(i); printf(" |"); //decoration
         for (int j = 0; j <= i; j++) {
             aff_value(tab[i][j]);
         }
@@ -52,15 +78,81 @@ void aff(int **tab, int n) {
     }
 }
 
+
+
+// C
+void triangle_it_vector(int n) {
+    Vector vec;
+    init_vector(&vec, n+1);
+
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = i; j >= 0; j--) {
+            if (j == 0) {
+                vec.tab[j] = 1;
+            }
+
+            else {
+                vec.tab[j] += vec.tab[j - 1];
+            }
+        }
+        vec.size += 1;
+        print_vector_tab(&vec);
+        printf("\n");
+    }
+}
+
+
+
+
+
+// D
+int factorielle_rec(int x) {
+    if (x <= 1) return 1;
+
+    return x * factorielle_rec(x-1);
+}
+
+
+// int factorielle(int x) {
+//     int result = 1;
+
+//     for (int i = 2; i <= x; i++) result *= i;
+//     return result;
+// }
+
+void triangle_fact(int n) {
+
+    aff_value(1); //k == 0 && n == 0
+    printf("\n");
+    for (int i = 1; i <= n; i++) {
+        aff_value(1); //k == 0
+        for (int j = 1; j < i; j++) {
+            aff_value(factorielle_rec(i) / (factorielle_rec(j) * factorielle_rec(i-j)));
+        }
+        aff_value(1);
+        printf("\n");
+    }
+}
+
+
+
 // =====================
 
 
 
 
 
-void A(int k, int n) {
+void A(int n) {
     printf("\nEXO2 A\n");
-    printf("%d\n", triangle_rec(k, n));
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= i; j++) {
+            int value = triangle_rec(j, i);
+            aff_value(value);
+        }
+        printf("\n");
+    }
 }
 
 void B(int n) {
@@ -74,26 +166,33 @@ void B(int n) {
     
     
     
-    triangle_it(tab, n);
-    aff(tab, n);
+    triangle_it_2d(tab, n);
+    print_tab_2d(tab, n);
 }
 
-void C(int k, int n) {
+void C(int n) {
     printf("\nEXO2 C\n");
-    printf("NOT WRITE\n");
+    triangle_it_vector(n);
+}
+
+
+void D(int n) {
+    printf("\nEXO2 D\n");
+    triangle_fact(n);
 }
 
 
 void exo2(void) {
     
-    int n = 6;
+    int n = 7;
     int k = 3;
     
-    printf("n = %d, k = %d\n", n, k);
+    printf("k = %d, n = %d\n", k, n);
 
-    A(k, n);
+    A(n);
     B(n);
-    C(k, n);
+    C(n);
+    D(n);
     
 }
 
