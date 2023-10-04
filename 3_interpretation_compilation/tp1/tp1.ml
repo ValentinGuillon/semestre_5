cf70dc781e29175515a7f9b3bee903eedfefbba1
Valentin_G <valentin.guillon93430@gmail.com>
2023-10-04 07:17:56 +0200

    inter et compil, tp1: tentative exo3 (incomplet, mais ça marche)

--- 3_interpretation_compilation/tp1/tp1.ml
+++ 3_interpretation_compilation/tp1/tp1.ml
@@ -4,47 +4,125 @@
 type expr = 
 | Num of int
 | Add of expr * expr
-| Diff of expr * expr
-| Prod of expr * expr
-| Quot of expr * expr
+| Sub of expr * expr
+| Mul of expr * expr
+| Div of expr * expr
 | Mod of expr * expr
 ;;
+(*
+let rec format e = 
+    match e with
+    | Num n -> Printf.sprintf "%d" n
+    | Add (a, b) -> Printf.sprintf "(%s + %s)" (format a) (format b)
+    | Sub (a, b) -> Printf.sprintf "(%s - %s)" (format a) (format b)
+    | Mul (a, b) -> Printf.sprintf "(%s * %s)" (format a) (format b)
+    | Div (a, b) -> Printf.sprintf "(%s / %s)" (format a) (format b)
+    | Mod (a, b) -> Printf.sprintf "(%s mod %s)" (format a) (format b)
+;;
+
+*)
+(*
+let rec format e = 
+    match e with
+    | Num n -> Printf.sprintf "%d" n
+    | Add (a, b) -> Printf.sprintf "%s + %s" (format a) (format b)
+    | Sub (a, b) -> Printf.sprintf "%s - %s" (format a) (format b)
+    | Mul (a, b) ->
+        match a with
+        | Num _ ->
+            match b with
+            | Add (_, _) | Sub (_, _) -> Printf.sprintf "%s * (%s)" (format a) (format b)
+            | _ -> Printf.sprintf "%s * %s" (format a) (format b)
+        | Add (_, _) | Sub (_, _) ->
+            match b with
+            | Num _ -> Printf.sprintf "(%s) * %s" (format a) (format b)
+            | Add (_, _) | Sub (_, _) -> Printf.sprintf "(%s) * (%s)" (format a) (format b)
+            | _ -> Printf.sprintf "%s * %s" (format a) (format b)
+        | _ -> Printf.sprintf "%s * %s" (format a) (format b)
+    
+
+    | Div (a, b) -> Printf.sprintf "(%s / %s)" (format a) (format b)
+    | Mod (a, b) -> Printf.sprintf "(%s mod %s)" (format a) (format b)
+;;
+*)
+
+
 
 let rec format e = 
     match e with
-        | Num n -> Printf.sprintf "%d" n
-        | Add (a, b) -> Printf.sprintf "(%s + %s)" (format a) (format b)
-        | Diff (a, b) -> Printf.sprintf "(%s - %s)" (format a) (format b)
-        | Prod (a, b) -> Printf.sprintf "(%s * %s)" (format a) (format b)
-        | Quot (a, b) -> Printf.sprintf "(%s / %s)" (format a) (format b)
-        | Mod (a, b) -> Printf.sprintf "(%s mod %s)" (format a) (format b)
+    | Num n -> Printf.sprintf "%d" n
+    | Add (a, b) -> Printf.sprintf "%s + %s" (format a) (format b)
+    | Sub (a, b) -> Printf.sprintf "%s - %s" (format a) (format b)
+    | Mul (a, b) ->
+        match a with
+        | Num _ ->
+            match b with
+            | Add (_, _) | Sub (_, _) -> Printf.sprintf "%s * (%s)" (format a) (format b)
+            | _ -> Printf.sprintf "%s * %s" (format a) (format b)
+        | Add (_, _) | Sub (_, _) ->
+            match b with
+            | Num _ -> Printf.sprintf "(%s) * %s" (format a) (format b)
+            | Add (_, _) | Sub (_, _) -> Printf.sprintf "(%s) * (%s)" (format a) (format b)
+            | _ -> Printf.sprintf "%s * %s" (format a) (format b)
+        | _ -> Printf.sprintf "%s * %s" (format a) (format b)
+    
+
+    | Div (a, b) -> Printf.sprintf "(%s / %s)" (format a) (format b)
+    | Mod (a, b) -> Printf.sprintf "(%s mod %s)" (format a) (format b)
 ;;
+
+
+
+
+
 
 let rec eval e =
     match e with
-        | Num n -> n
-        | Add (a, b) -> eval(a) + eval(b)
-        | Diff (a, b) -> eval(a) - eval(b)
-        | Prod (a, b) -> eval(a) * eval(b)
-        | Quot (a, b) -> eval(a) / eval(b)
-        | Mod (a, b) -> eval(a) mod eval(b)
+    | Num n -> n
+    | Add (a, b) -> eval(a) + eval(b)
+    | Sub (a, b) -> eval(a) - eval(b)
+    | Mul (a, b) -> eval(a) * eval(b)
+    | Div (a, b) -> eval(a) / eval(b)
+    | Mod (a, b) -> eval(a) mod eval(b)
 ;;
 
 
-let evalIntoFormat e = format e ^ " = " ^ format(Num (eval e))
+let formatIntoEval e = format e ^ " = " ^ format(Num (eval e))
 ;;
 
 
-
-let e = Diff(Add(Prod(Mod(Num 8, Num 3), Num 2), Num 3), Num 4)
+(*
+let e = Sub(Add(Mul(Mod(Num 8, Num 3), Num 2), Num 3), Num 4)
 ;;
-(* let e = Prod(Num 3, Add(Num 9, Num 8))
-;; *)
+*)
+(*
+let e = Mul(Num 3, Add(Num 9, Num 8))
+;;
+*)
 
-print_endline(evalIntoFormat e)
+(*
+print_endline(formatIntoEval e)
+;;
+*)
+
+let e = Mul (Num 1, Add (Num 2, Num 3))
 ;;
 
+let e1 = Add (Num 1, Add (Num 2, Num 3))
+;;
+let e2 = Add (Num 1, Mul (Num 2, Num 3))
+;;
+let e3 = Mul (Num 1, Add (Num 2, Sub (Num 3, Num 4)))
+;;
 
+print_endline(format e)
+;;
 
+print_endline(format e1)
+;;
+print_endline(format e2)
+;;
+print_endline(format e3)
+;;
 
 
